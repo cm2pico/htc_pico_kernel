a3c9ea3f709b22036d1c954a71cf060a13c1ed6a
sakindia123 <sa6915@gmail.com>
2012-09-08 21:21:30 +0530

    mach-msm: Updates from Golfu

--- arch/arm/mach-msm/htc_awb_cal.c
+++ arch/arm/mach-msm/htc_awb_cal.c
@@ -26,13 +26,8 @@ GNU General Public License for more details.
 
 /* configuration tags specific to msm */
 #define ATAG_MSM_AWB_CAL	0x59504550 /* MSM CAMERA AWB Calibration */
-/* HTC_START */
-#if defined(CONFIG_ARCH_MSM7X30) || defined(CONFIG_ARCH_MSM8X60) || defined(CONFIG_ARCH_MSM7X27A)
-/* HTC_END */
+
 #define AWB_CAL_MAX_SIZE	0x1000U     /* 0x1000 = 4096 bytes */
-#else
-#define AWB_CAL_MAX_SIZE	0x800U     /* 0x800 = 2048 bytes */
-#endif
 
 struct qct_lsc_struct{
 	unsigned long int	lsc_verify;
